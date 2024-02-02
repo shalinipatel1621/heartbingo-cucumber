f8dfe6b0c2c3d81407463ec5f13d0f5b003da5fe
shalinipatel1621 <shalinipatel1621@gmail.com>
2024-02-02 09:10:13 +0000

    heartbingo

---
+++ src/test/java/uk/co/heartbingo/runners/RunCukeTest.java
@@ -0,0 +1,17 @@
+package uk.co.heartbingo.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "uk/co/heartbingo/steps",
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "json:target/cucumber-reports/cucumber.json"},
+        tags = "@smoke"
+)
+public class RunCukeTest extends AbstractTestNGCucumberTests {
+
+
+}
---
+++ src/test/java/uk/co/heartbingo/steps/Hooks.java
@@ -0,0 +1,25 @@
+package uk.co.heartbingo.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import uk.co.heartbingo.propertyreader.PropertyReader;
+import uk.co.heartbingo.utility.Utility;
+
+
+public class Hooks extends Utility {
+
+    @Before
+    public void setUp(){
+        selectBrowser(PropertyReader.getInstance().getProperty("browser"));
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        if (scenario.isFailed()){
+            final  byte[]  screenshot = getScreenShot();
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+        //closeBrowser();
+    }
+}
